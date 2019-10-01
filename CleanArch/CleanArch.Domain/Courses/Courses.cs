9b4dc3ede1c55abce5efc3ec6fbbd7741989fdcb
Srikanth Rao <srikanthsdrao@gmail.com>
2019-10-01 11:25:53 +0530

    Initial Course domain model

---
+++ CleanArch/CleanArch.Domain/Courses/Courses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Courses
+{
+    public class Courses
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+
+        public string CourceDesc { get; set; }
+        public string ImageURL { get; set; }
+
+    }
+}
